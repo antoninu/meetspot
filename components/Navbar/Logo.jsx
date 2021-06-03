23a6e6bc87c3bd8c5baa004cb994ffb12b2af534
Isabela <im.sarmieento@uniandes.edu.co>
2021-06-03 17:00:51 -0500

    Home Link

--- components/Navbar/Logo.jsx
+++ components/Navbar/Logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Center, Img } from '@chakra-ui/react';
+import { Center, Button } from '@chakra-ui/react';
 import Logo from 'public/logo.svg';
+import NextLink from 'next/link';
 
 export default function LogoSection() {
   return (
     <Center ml={[0, 14]} h={5} w={200}>
-      <Logo />
+      <NextLink href="/calendar">
+        <Logo style={{ cursor: 'pointer' }} />
+      </NextLink>
     </Center>
   );
 }
