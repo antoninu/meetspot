86e58821d3e7787605aa56d862650454f3c4af4f
Antonio Aspite <a.aspite@uniandes.edu.co>
2021-06-04 09:48:31 -0500

    cursor pointer over logo

--- components/Navbar/Logo.jsx
+++ components/Navbar/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Center, Button } from '@chakra-ui/react';
+import { Box, Center } from '@chakra-ui/react';
 import Logo from 'public/logo.svg';
 import NextLink from 'next/link';
 
 export default function LogoSection() {
   return (
-    <Center ml={[0, 14]} h={5} w={200}>
-      <NextLink href="/calendar" style={{ cursor: 'pointer' }}>
+    <Center ml={[0, 14]} h={5} w={200} cursor="pointer">
+      <NextLink href="/calendar">
         <Logo />
       </NextLink>
     </Center>
