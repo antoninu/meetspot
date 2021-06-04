fd316577de5bd9c9fa85cce4414dbcf6805ee569
Antonio Aspite <a.aspite@uniandes.edu.co>
2021-06-04 12:03:31 -0500

    fat faw footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Center } from '@chakra-ui/react';
+import { Center, Heading, Text } from '@chakra-ui/react';
 import Section from 'components/shared/Section';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
@@ -16,7 +16,24 @@ export default function Footer() {
   return (
     <footer>
       <hr />
-      <Section p={5}>
+      <Section p={5} textAlign="center">
+        <Heading mb={4}>FAQ</Heading>
+        <Heading as="h4" size="md">
+          Do I have to pay for the service?
+        </Heading>
+        <Text mb={2}>Not necessary for now.</Text>
+        <Heading as="h4" size="md">
+          Do my partners need to have an account to access this service?
+        </Heading>
+        <Text mb={2}>
+          Yes they do in order for us to let you know their availabilities.
+        </Text>
+        <Heading as="h4" size="md">
+          Can I load my schedule with my current Google Calendar?
+        </Heading>
+        <Text mb={10}>
+          Not for the moment, but it will be an incoming feature.
+        </Text>
         <Center>© 2021 Meetspot. Todos los derechos reservados</Center>
         <Center>
           <Link href="#" locale={spanish ? 'en' : 'es-CO'}>
--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -21,32 +21,6 @@ const Hero = () => {
           <Img alt="HERO_IAMGE" src="/images/meetspothero.webp" />
         </Box>
       </Flex>
-      <div
-        ml={[0, 14]}
-        align="center"
-        h="100%"
-        px={[7, 7, 0]}
-        py={[14]}
-        minH="80vh"
-      >
-        <Heading mb={4}>FAQ</Heading>
-        <Heading as="h4" size="md">
-          Do I have to pay for the service?
-        </Heading>
-        <Text mb={2}>Not necessary for now.</Text>
-        <Heading as="h4" size="md">
-          Do my partners need to have an account to access this service?
-        </Heading>
-        <Text mb={2}>
-          Yes they do in order for us to let you know their availabilities.
-        </Text>
-        <Heading as="h4" size="md">
-          Can I load my schedule with my current Google Calendar?
-        </Heading>
-        <Text mb={10}>
-          Not for the moment, but it will be an incoming feature.
-        </Text>
-      </div>
     </>
   );
 };
